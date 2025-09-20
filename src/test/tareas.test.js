b3bd3beaecc46544de71f3d8bce38520e1ae0402
Josthin Paz <josthinpaz2@gmail.com>
2025-09-20 11:25:28 -0500

    Subiendo git action
    Co-authored-by: alyconr <alyconr@hotmail.com>

--- src/test/tareas.test.js
+++ src/test/tareas.test.js
@@ -1,5 +1,4 @@
 /* eslint-env jest */
-/* eslint-env jest */
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
