d7303ddedb73b0c8a969c110e93b745a8e9b8714
Camilo <camiloandrespinzonmelendrez@gmail.com>
2025-09-21 17:07:09 -0500

    Subiendo git action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base : "/Todo_list_App_/",
+  // Use a relative path for local serving
+  base: "./",
   plugins: [react()],
   test: {
     environment: "jsdom",
