86cc09ee394b1584f99eccaca47b622ccc42885d
Camilo <camiloandrespinzonmelendrez@gmail.com>
2025-09-21 15:55:29 -0500

    2subiendo cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion"; // <-- AGREGA ESTA LÍNEA
 import { usuarios } from "../db";
 import SearchInput from "./pages/buscador";
 import Listas from "./pages/listas";
