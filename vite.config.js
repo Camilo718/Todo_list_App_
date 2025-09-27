4e5980e125ce777d4425e1e0b567c89b229722d9
Camilo <camiloandrespinzonmelendrez@gmail.com>
2025-09-27 07:20:55 -0500

    cambios finales

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // Use a relative path for local serving
-  base: "./",
+  base: "/Todo_list_App/",
   plugins: [react()],
   test: {
     environment: "jsdom",
